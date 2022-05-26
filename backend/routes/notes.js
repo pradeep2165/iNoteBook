35d3edc805df1e2bdc1f00258ff28620124f79f4
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-05-26 15:58:11 +0530

    update api created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -34,4 +34,36 @@ router.post("/addnote", fetchuser, [body("title", "Title should be atleast 3 cha
   }
 });
 
+//Route 3: Updating a existing note using PUT:/api/note/update_note .login required
+router.put("/update_note/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+
+    const newNotes = {};
+    if (title) {
+      newNotes.title = title;
+    }
+    if (description) {
+      newNotes.description = description;
+    }
+    if (tag) {
+      newNotes.tag = tag;
+    }
+    //find the note to be updated and update
+    let notes = await Notes.findById(req.params.id);
+    if (!notes) {
+      return res.status(404).send("Not Found");
+    }
+    if (notes.user.toString() !== req.user.id) {
+      return res.status(404).send("Not Allowed");
+    }
+    notes = await Notes.findByIdAndUpdate(req.params.id, { $set: newNotes }, { new: true });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    //for log in response
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
