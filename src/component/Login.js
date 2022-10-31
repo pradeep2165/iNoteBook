b1257ffff5fddc444a6896035f57d16bf2b07f2b
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-31 20:08:44 +0530

    login and logout component formated

--- src/component/Login.js
+++ src/component/Login.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import noteContext from "../context/note/noteContext";
 
 export default function Login() {
-  const { showAlert } = useContext(noteContext);
+  const { showAlert, darkMode } = useContext(noteContext);
   let navigate = useNavigate();
   const [data, setData] = useState({
     email: "",
@@ -33,26 +33,37 @@ export default function Login() {
     }
   };
   return (
-    <div className="container my-3">
-      <form onSubmit={handelSubmit}>
-        <h3>Login </h3>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
+    <div className={`bg-${darkMode ? "dark":"primary"} bg-opacity-${darkMode ? "1":"10"} d-flex justify-content-center`} style={{height:"100vh"}}>
+
+    <div className="col-md-4 col-12">
+      <form onSubmit={handelSubmit} className={`border border-1 rounded-3 p-4 border-success bg-${darkMode ? "secondary" : "primary" } bg-opacity-25` }>
+        <h3 className={`text-center text-${darkMode ? "light":"dark"}`}>Login </h3>
+        <hr className={`text-${darkMode ? 'light':'dark'}`}/>
+
+        <div className="mb-3 d-flex justify-content-end gap-1 align-items-center">        
+          <label htmlFor="exampleInputEmail1" className={`form-label text-${darkMode ? "light":"dark"}`}>
             Email address
           </label>
+          <div>
           <input type="email" className="form-control" name="email" aria-describedby="emailHelp" onChange={onChange} autoComplete="off" required />
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
+        
+        <div className="mb-3 d-flex justify-content-end gap-1 align-items-center">
+          <label htmlFor="exampleInputPassword1" className={`form-label text-${darkMode ? "light":"dark"}`}>
             Password
           </label>
+          <div>
           <input type="password" className="form-control" name="password" onChange={onChange} autoComplete="off" required />
+          </div>
         </div>
-
+        <div className="d-flex justify-content-end">
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        </div>
       </form>
+    </div>
     </div>
   );
 }
--- src/component/Signup.js
+++ src/component/Signup.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import noteContext from "../context/note/noteContext";
 
 export default function Signup() {
-  const { showAlert } = useContext(noteContext);
+  const { showAlert, darkMode } = useContext(noteContext);
   let navigate = useNavigate();
   const [data, setData] = useState({
     name: "",
@@ -43,38 +43,53 @@ export default function Signup() {
     }
   };
   return (
-    <div className="container my-3">
-      <form onSubmit={handelSubmit} autoComplete="off">
-        <h3>Singup </h3>
-        <div className="mb-3">
-          <label htmlFor="Name" className="form-label">
+    <div className={`bg-${darkMode ? "dark":"primary"} bg-opacity-${darkMode ? "1":"10"} d-flex justify-content-center`} style={{height:"100vh"}}>
+      <div className="col-md-4 col-12">
+      <form onSubmit={handelSubmit} autoComplete="off" className={`border border-1 rounded-3 p-4 border-success bg-${darkMode ? "secondary" : "primary" } bg-opacity-25` }>
+        <h3 className={`text-center text-${darkMode ? "light":"dark"}`}>Singup </h3>
+        <hr className={`text-${darkMode ? 'light':'dark'}`}/>
+        <div className="mb-3 d-flex justify-content-end gap-1 align-items-center">
+          <label htmlFor="Name" className={`form-label text-${darkMode ? "light":"dark"}`}>
             Name
           </label>
+          <div>
           <input type="text" className="form-control" name="name" onChange={onChange} minLength={3} required />
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
+        <div className="mb-3 d-flex justify-content-end gap-1 align-items-center">
+          <label htmlFor="exampleInputEmail1" className={`form-label text-${darkMode ? "light":"dark"}`}>
             Email address
           </label>
+          <div>
           <input type="email" className="form-control" name="email" aria-describedby="emailHelp" onChange={onChange} minLength={5} required />
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
+        <div className="mb-3 d-flex justify-content-end gap-1 align-items-center">
+          <label htmlFor="exampleInputPassword1" className={`form-label text-end text-${darkMode ? "light":"dark"}`}>
             Password
           </label>
+          <div>
+
           <input type="password" className="form-control" name="password" onChange={onChange} autoComplete="off" minLength={5} required />
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
+        <div className="mb-3 d-flex justify-content-end gap-1 align-items-center">
+          <div>
+            <label className={`form-label text-end text-${darkMode ? "light":"dark"}`}>
             Confirm Password
           </label>
+          </div>
+          <div>
           <input type="password" className="form-control" name="cpassword" onChange={onChange} autoComplete="off" minLength={5} required />
+          </div>
         </div>
-
-        <button type="submit" className="btn btn-primary">
+      <div className="d-flex justify-content-end">
+      <button type="submit" className="btn btn-primary">
           Submit
         </button>
+      </div>
       </form>
+      </div>
     </div>
   );
 }
